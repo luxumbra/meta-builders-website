61b383b6ab6853e345c896ad8757df0d63b93bf0
luxumbra <me@luxumbra.dev>
2022-08-19 16:55:57 +0100

    title for partner links

--- src/components/Cards/PartnersCard.tsx
+++ src/components/Cards/PartnersCard.tsx
@@ -15,7 +15,7 @@ export function PartnersCard({ partner }: {partner: PartnersCardProperties}): JS
   const partnerImage = image !== '' ? image : 'missing-image.png';
 
   return (
-    <a className="partners-card group aspect-video leadIn flex-shrink" href={url}>
+    <a className="partners-card group aspect-video leadIn flex-shrink" href={url} title={`Visit ${name}`}>
     <figure className="relative w-full h-full items-center text-center flex flex-row content-center justify-center">
         <picture
           className={`
@@ -39,7 +39,7 @@ export function PartnersCard({ partner }: {partner: PartnersCardProperties}): JS
 
             />
           ) : ( */}
-            <img src={`/assets/partners/images/${partnerImage}`} alt={`${name}'s logo`} className=' object-contain w-auto h-full transition-all duration-300 bg-cover opacity-70 group-hover:scale-110 group-hover:opacity-50 group-focus:scale-110 group-focus:opacity-50 filter grayscale invert dark:invert-0 shadow-teal-400' loading="lazy" />
+            <img src={`/assets/partners/images/${partnerImage}`} alt={`${name}'s logo`} className=' object-contain w-auto h-full transition-all duration-300 bg-cover opacity-70 group-hover:scale-110 group-hover:opacity-100 group-focus:scale-110 group-focus:opacity-100 filter grayscale invert dark:invert-0 shadow-teal-400' loading="lazy" />
         {/* )} */}
 
       </picture>
