41f0ebed261ac9f8cf1f6407e7e13622df49b711
luxumbra <me@luxumbra.dev>
2022-09-03 04:27:17 +0100

    adding display of mymeta if member uses a metagame.wtf url

--- src/components/Cards/TeamMemberCard.tsx
+++ src/components/Cards/TeamMemberCard.tsx
@@ -3,12 +3,14 @@ import type { ITeamMember } from '~mb/types';
 export type TeamMember = ITeamMember
 
 export function TeamMemberCard({ member }: { member: TeamMember }): JSX.Element {
-  const { image, name, role } = member;
+  const { image, name, role, twitter, linkedin, bio, email } = member;
   // const sizes = "(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw";
   // const siteUrl = import.meta.env.VITE_SITE_URL as string;
   // const avatar = new URL(image ?? 'missing-image.png', `${siteUrl}/assets/team/images`);
   const avatar = image !== '' ? image : 'missing-image.png';
-  // const shouldCenter = !!(bio && bio.length <= 50);
+  const shouldCenter = !!(bio && bio.length <= 50);
+  const url = new URL(linkedin ?? '', import.meta.url);
+  const isMyMeta = url.hostname === 'metagame.wtf';
 
   return (
     <div className="group relative pt-0">
@@ -39,19 +41,26 @@ export function TeamMemberCard({ member }: { member: TeamMember }): JSX.Element 
           <h3 className="text-sm 2xl:text-base font-normal text-slate-600 dark:text-slate-400">{name}</h3>
           <p className="text-xxs 2xl:text-xs ">{role}</p>
         </figcaption>
-        {/* <div className="absolute rounded-lg bottom-0 flex flex-col items-start text-left justify-start content-center min-w-full md:w-[135%] flex-grow min-h-[120%] h-auto gap-2 transition-all duration-300 opacity-0 group-hover:opacity-100 group-focus:opacity-100 scale-50 group-hover:scale-100 group-focus:scale-100 group-hover:-translate-y-[0%] group-focus:translate-y-0 translate-y-10 origin-center z-50  filter backdrop-blur-xl ">
+        <div className="absolute rounded-lg bottom-0 flex flex-col items-start text-left justify-start content-center min-w-full md:w-[135%] flex-grow min-h-[120%] h-auto gap-2 transition-all duration-300 opacity-0 group-hover:opacity-100 group-focus:opacity-100 scale-50 group-hover:scale-100 group-focus:scale-100 group-hover:-translate-y-[0%] group-focus:translate-y-0 translate-y-10 origin-center z-50  filter backdrop-blur-xl ">
+
           <div className="relative bio-content flex flex-col gap-y-2 p-3 flex-grow-0 z-10 w-full">
+
+          <div className=" flex-grow-[3] flex flex-col gap-y-2 h-auto">
             <h4 className="text-lg font-bold gradient-text tracking-normal text-center">{name}</h4>
             {bio ? <p className={`text-xs text-slate-500 dark:text-white leading-tight ${shouldCenter ? 'text-center' : 'text-left'}  `}>{bio}</p> : undefined}
+         </div>
+        <div className="inline-flex gap-2 flex-row justify-self-end items-center flex-shrink-0 translate-y-5 transition-transform  group-hover:translate-y-0 -translate-x-6 group-hover:translate-x-0 scale-0 group-hover:scale-100">
 
-            <div className="flex gap-2 flex-row justify-self-end items-center flex-shrink-0 translate-y-5 transition-transform  group-hover:translate-y-0 -translate-x-6 group-hover:translate-x-0 scale-0 group-hover:scale-100">
               {twitter ? <a href={twitter} title={`${name} on Twitter`} className="text-xs text-violet-700 dark:text-white badge badge-link">Twitter</a> : undefined}
-              {linkedin ? <a href={linkedin} title={`${name} on Twitter`} className="text-xs text-violet-700 dark:text-white badge badge-link">LinkedIn</a> : undefined}
+
+              {linkedin ? <a href={linkedin} title={`${name} on ${isMyMeta ? 'MyMeta Profile' : 'LinkedIn'}`} className="text-xs text-violet-700 dark:text-white badge badge-link">{isMyMeta ? 'MyMeta Profile' : 'LinkedIn'}</a> : undefined}
+
               {email ? <a href={email} title={`${name} on Email`} className="text-xs text-violet-700 dark:text-white badge badge-link">Email</a> : undefined}
+
             </div>
           </div>
           <div className='absolute inset-0  bg-slate-100 dark:bg-slate-900 opacity-80 filter backdrop-blur-xl rounded-lg shadow-lg p-4 w-full h-full z-0'/>
-        </div> */}
+        </div>
       </figure>
     </div>
   )
