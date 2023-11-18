c3d17948abb55e40e3924a51cfccefb2d66c6eff
luxumbra <me@luxumbra.dev>
2023-11-18 16:52:38 +0000

    Fix/broken metadata (#24)
    * wip on upgrading
    
    * fix broken build and font resolution issue. Still b0rked on the nft metadata issue
    
    * adding temp notice for marketplace while we sort the metadata issues.
    
    * contracts under construction update.
    
    * some accessibility improvements

--- src/components/Default/Footer.tsx
+++ src/components/Default/Footer.tsx
@@ -1,66 +1,80 @@
+import { Icon } from '@iconify/react'
+import { DateTime } from 'luxon'
+import { HashLink } from 'react-router-hash-link'
+import { v4 as uuid } from 'uuid'
 
-import { Icon } from "@iconify/react";
-import { DateTime } from "luxon";
-import { HashLink } from "react-router-hash-link";
-import { v4 as uuid } from "uuid";
-
-import { useScrollReveal } from "~mb/hooks/animation";
+import { useScrollReveal } from '~mb/hooks/animation'
 
 export const links = [
   {
-    url: "https://meta-builders.com/",
+    url: 'https://meta-builders.com/',
     description: "Meta-Builders' official website",
-    icon: "mdi:home",
+    icon: 'mdi:home'
   },
   {
-    url: "https://www.youtube.com/channel/UCEnYtm1vjjbFajROeIsRofA",
-    description: "Meta-Builders on GitHub",
-    icon: "mdi:youtube",
+    url: 'https://www.youtube.com/channel/UCEnYtm1vjjbFajROeIsRofA',
+    description: 'Meta-Builders on GitHub',
+    icon: 'mdi:youtube'
   },
   {
-    url: "https://discord.gg/metabuilders",
-    description: "Meta-Builders on Discord",
-    icon: "mdi:discord",
+    url: 'https://discord.gg/metabuilders',
+    description: 'Meta-Builders on Discord',
+    icon: 'mdi:discord'
   },
   {
-    url: "https://twitter.com/meta_builders",
-    description: "Meta-Builders on Twitter",
-    icon: "mdi:twitter",
-  },
-];
+    url: 'https://twitter.com/meta_builders',
+    description: 'Meta-Builders on Twitter',
+    icon: 'mdi:twitter'
+  }
+]
 
 export function CopyrightNotice(): JSX.Element {
-  const dt = DateTime.local();
-  const currentYear = dt.year;
+  const dt = DateTime.local()
+  const currentYear = dt.year
 
-  return <p className="text-xs leadIn invisible xl:self-end gradient-text">Copyright &copy; {currentYear} Meta-Builders</p>
+  return (
+    <p className='leadIn gradient-text invisible text-xs xl:self-end'>
+      Copyright &copy; {currentYear} Meta-Builders
+    </p>
+  )
 }
 
 export function Footer(): JSX.Element {
-  const elementId = ".leadIn";
-  const triggerId = "#footer";
+  const elementId = '.leadIn'
+  const triggerId = '#footer'
 
-  useScrollReveal(elementId, triggerId);
+  useScrollReveal(elementId, triggerId)
 
   return (
-    <footer id="footer" className="hidden lg:flex relative overflow-y-hidden  flex-col items-center justify-center h-32 lg:h-64 w-full pointer-events-none z-10">
-      <div className="footer-main w-3/4  leadIn invisible pointer-events-auto">
-        <HashLink to="/#home" className="font-black font-display text-sm text-shadow-alt"><strong className="gradient-text">Meta-Builders</strong></HashLink>
+    <footer
+      id='footer'
+      className='pointer-events-none relative z-10 hidden  h-32 w-full flex-col items-center justify-center overflow-y-hidden lg:flex lg:h-48 2xl:h-64'
+    >
+      <div className='footer-main leadIn  pointer-events-auto invisible w-3/4'>
+        <HashLink
+          to='/#home'
+          className='text-shadow-alt font-display font-black text-sm'
+        >
+          <strong className='gradient-text'>Meta-Builders</strong>
+        </HashLink>
       </div>
-      <div className="footer-aside w-3/4 flex flex-col lg:flex-row items-center justify-between pointer-events-auto">
+      <div className='footer-aside pointer-events-auto flex w-3/4 flex-col items-center justify-between lg:flex-row'>
         <CopyrightNotice />
 
-        <ul className="mb-urls relative grid grid-cols-4 w-full gap-4 lg:w-1/4">
-          {links.map((link) => (
-            <li key={uuid()} className="leadIn invisible">
+        <ul className='mb-urls relative grid w-full grid-cols-4 gap-4 lg:w-1/4'>
+          {links.map(link => (
+            <li key={uuid()} className='leadIn invisible'>
               <a
-                className="group w-12 2xl:w-16 h-12 2xl:h-16 p-2 2xl:p-3 border-2 border-violet-800 dark:border-teal-200 rounded-full inline-flex items-center justify-center text-shadow-alt-sm dark:text-shadow-alt-sm-teal hover:animate-pulse"
+                className='text-shadow-alt-sm dark:text-shadow-alt-sm-teal group inline-flex h-12 w-12 items-center justify-center rounded-full border-2 border-violet-800 p-2 hover:animate-pulse dark:border-teal-200 2xl:h-16 2xl:w-16 2xl:p-3'
                 href={link.url}
-                target="_blank"
-                rel="noreferrer"
+                target='_blank'
+                rel='noreferrer'
               >
-                <span className="sr-only">{link.description}</span>
-                <Icon className="w-full h-full text-violet-800  transition-colors text-6xl dark:text-teal-300 " icon={link.icon} />
+                <span className='sr-only'>{link.description}</span>
+                <Icon
+                  className='h-full w-full text-violet-800  transition-colors text-6xl dark:text-teal-300 '
+                  icon={link.icon}
+                />
               </a>
             </li>
           ))}
@@ -70,4 +84,4 @@ export function Footer(): JSX.Element {
   )
 }
 
-export default Footer;
+export default Footer
--- src/components/Rain.tsx
+++ src/components/Rain.tsx
@@ -1,46 +1,61 @@
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect } from 'react'
 
-import { rain } from "~mb/animation/rain";
-import { useDarkMode } from "~mb/lib/hooks";
-
+import { rain } from '~mb/animation/rain'
+import { useDarkMode } from '~mb/lib/hooks'
 
 export function Rain({
   blur,
   masked,
   effectOpacity,
-  z,
+  z
 }: {
-  blur?: boolean;
-  masked?: boolean;
-  effectOpacity?: string;
-  z?: number;
+  blur?: boolean
+  masked?: boolean
+  effectOpacity?: string
+  z?: number
 }): JSX.Element {
-
+  const prefersReductedMotion = window.matchMedia(
+    '(prefers-reduced-motion: reduce)'
+  ).matches
   const { isDarkMode } = useDarkMode()
   const initCallback = useCallback(() => {
-    rain(isDarkMode)
-  }, [isDarkMode])
+    rain(isDarkMode, prefersReductedMotion)
+  }, [isDarkMode, prefersReductedMotion])
 
   useEffect(() => {
     initCallback()
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
-    rain(isDarkMode)
-  } , [isDarkMode])
+    rain(isDarkMode, prefersReductedMotion)
+  }, [isDarkMode, prefersReductedMotion])
 
   return (
-    <div data-id="rain" className={`rain fixed inset-0 w-full h-full z-0 opacity-50 motion-reduce:hidden pointer-events-none opacity-${effectOpacity ?? '50' }`}>
-      {blur ? <div className="absolute inset-0 w-full h-full z-10 bg-white bg-opacity-20 dark:bg-opacity-100 dark:bg-glass-primary-900 backdrop-blur-md dark:backdrop-blur-md pointer-events-none" /> : undefined}
+    <div
+      data-id='rain'
+      className={`rain pointer-events-none fixed inset-0 z-0 h-full w-full opacity-50 motion-reduce:hidden opacity-${
+        effectOpacity ?? '50'
+      }`}
+    >
+      {blur ? (
+        <div className='pointer-events-none absolute inset-0 z-10 h-full w-full bg-white bg-opacity-20 backdrop-blur-md dark:bg-glass-primary-900 dark:bg-opacity-100 dark:backdrop-blur-md' />
+      ) : undefined}
 
       <div
-          className={`absolute inset-0 w-full h-full pointer-events-none ${z ? z + 5 : 5}`}
-          style={{boxShadow: '0 0 250px 100px black inset'}}
-        />
+        className={`pointer-events-none absolute inset-0 h-full w-full ${
+          z ? z + 5 : 5
+        }`}
+        style={{ boxShadow: '0 0 250px 100px black inset' }}
+      />
 
-      <canvas data-id="rain-canvas" className={`rain-canvas absolute ${!masked ? 'z-0' : 'z-10'} opacity-50 pointer-events-none`}/>
+      <canvas
+        data-id='rain-canvas'
+        className={`rain-canvas absolute ${
+          !masked ? 'z-0' : 'z-10'
+        } pointer-events-none opacity-50`}
+      />
     </div>
   )
 }
